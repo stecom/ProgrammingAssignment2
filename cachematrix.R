3d949f68778835965eda61d93e65edf873b64bc5
stecom <ENGLON93728LADM@ENGLON93728L.ad.infosys.com>
2014-06-21 22:54:26 +0200

    definition of the special matrix makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse. definition of cache solve - get  calculate if required

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,21 @@
 #################################################
 ##
 ##      function :makeCacheMatrix
-##	  function:cacheSolve
+##	  function :cacheSolve
 ##
 ################################################
 ##
-##function :makeCacheMatrix
+##function :  makeCacheMatrix
 ## The first function, makeCacheMatrix creates a special "matrix", 
 ## which is really a list containing a function to 
 ## 1.set the value of the vector
 ## 2.get the value of the vector
 ## 3.set the value of the invert
 ## 4.get the value of the invert
+## it contains the object with its accessors method
 
-
-##function:cacheSolve
+##function : cacheSolve
+## contains the logic the manage the special Natrix
 ##calculates the mean of the special "matrix" 
 ## created with the above function. 
 ## However, it first checks to see if the inverted matrix has already been calculated. 
@@ -23,14 +24,29 @@
 ##  For example, if X is a square invertible matrix, 
 ##     then solve(X) returns its inverse
 
-## Write a short comment describing this function
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        ## definition of the cache
+        cache_inv  <- NULL		# initialisation  of the cache
 
+        ## definition of the methods of this special Matrix
+        set <- function(y) {
+			## creation of the special "matrix" (constructor)
+                  x <<- y  			# set the data by overwriting
+                  cache_inv  <<- NULL	# erase cache
+
+        }
+        get <- function() x					# return the object
+        setinv <- function(inv) cache_inv <<- inv 	# set the cache
+        getinv <- function() cache_inv			# return the cache
+        list(set = set, get = get,				# return the object with its methods
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
